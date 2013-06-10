7c1cbe475b17012f296dc8be94d60e3d7e04d28b
Appertry <spotberry@Airbooks-MacBook-Air.local>
2013-06-10 13:15:52 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '23a2ec46bc5fe8811cc70c6d4b76982719e6bddde09424f21d4f1697034b9b43ad39831d301acd46a0bdff812649ed05a9a931e38dff86eead511e6c36bb948f'
